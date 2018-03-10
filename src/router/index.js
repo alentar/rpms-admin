66a3fe0e1b1d667a5c5f98dd272dbf0aae77f217
Kasun <alankasun@gmail.com>
2018-03-10 08:20:23 +0530

    router
    Signed-off-by: Kasun <alankasun@gmail.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Signin from '@/components/Authentication/Signin'
+import Dashboard from '@/components/Dashboard/Dashboard'
+import AuthGuard from './guards/auth'
+import GuestGuard from './guards/guest'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: Dashboard,
+      beforeEnter: AuthGuard,
+      meta: {
+        title: 'Dashboard'
+      }
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin,
+      beforeEnter: GuestGuard,
+      meta: {
+        title: 'Signin'
+      }
+    }
+  ],
   mode: 'history'
 })
