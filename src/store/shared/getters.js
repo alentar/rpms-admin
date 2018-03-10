f176d732071978ed20056c35ac9da569c60b6e69
Kasun <alankasun@gmail.com>
2018-03-10 08:20:50 +0530

    Shared Store
    Signed-off-by: Kasun <alankasun@gmail.com>

---
+++ src/store/shared/getters.js
@@ -0,0 +1,11 @@
+'use strict'
+
+export default {
+  layout (state) {
+    return state.layout
+  },
+
+  loading (state) {
+    return state.loading === true
+  }
+}
---
+++ src/store/shared/mutation-types.js
@@ -0,0 +1,4 @@
+'use strict'
+
+export const SET_LAYOUT = 'SET_LAYOUT' // set layout for app
+export const SET_LOADING = 'SET_LOADING' // sets app loading state
