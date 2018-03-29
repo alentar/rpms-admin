74ea12b10035f5a956f7ea7fd3d06b66278ddb9c
Kasun Vithanage <alankasun@gmail.com>
2018-03-29 18:25:57 +0530

    wards origins
    Signed-off-by: Kasun Vithanage <alankasun@gmail.com>

--- src/pages/Wards/Wards.vue
+++ src/pages/Wards/Wards.vue
@@ -1,13 +1,38 @@
 <template>
-  <div>Wards</div>
+  <v-container grid-list-md text-xs-center xs10>
+    <v-layout row wrap>
+      <v-flex xs3 v-for="i in 36" :key="`3${i}`">
+        <v-card dark color="secondary" height="200px" class="text-xs-center">
+          <v-card-text class="px-0">
+            <div class="display-2">Ward</div>
+            <div class="display-1">{{ i }}</div>
+            <div class="subheading">Ward Name</div>
+            <div class="body-2">Total beds</div>
+            <v-btn primary dark @click="">View</v-btn>
+          </v-card-text>
+
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-    export default {
-      name: 'wards'
+import rpms from '@/services/rpms'
+
+export default {
+  name: 'wards',
+
+  data () {
+    return {
+      wards: []
     }
+  },
+
+  methods: {
+    getWards () {
+
+    }
+  }
+}
 </script>
-
-<style scoped>
-
-</style>
---
+++ src/services/rpms/ward/index.js
@@ -0,0 +1,11 @@
+import api from '../api'
+import AuthService from '../auth'
+
+class Ward {
+  async create (number, name) {
+
+  }
+}
+
+const ward = new Ward()
+export default ward
