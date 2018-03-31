d7304877ac83ac8e7ba75bc19f26c3be4822fa23
Kasun Vithanage <alankasun@gmail.com>
2018-03-31 13:15:30 +0530

    Introducing global event bus
    Fixed major bug in token renewal
    
    Signed-off-by: Kasun Vithanage <alankasun@gmail.com>

--- src/services/rpms/user/index.js
+++ src/services/rpms/user/index.js
@@ -1,48 +1,48 @@
 import api from '../api'
-import AuthService from '../auth'
+import auth from '../auth'
 
-export default class User {
-  static async createUser (payload) {
+class User {
+  async createUser (payload) {
     return api().post('users', payload, {
       headers: {
-        Authorization: AuthService.accessToken
+        Authorization: auth.accessToken
       }
     })
     .then((res) => { return Promise.resolve(res.data.user) })
     .catch(err => { return Promise.reject(err.response.data) })
   }
 
-  static async updateUser (payload) {
+  async updateUser (payload) {
     return api().put(`users/${payload.id}`, payload, {
       headers: {
-        Authorization: AuthService.accessToken
+        Authorization: auth.accessToken
       }
     })
     .then((res) => { return Promise.resolve(res.data.user) })
     .catch(err => { return Promise.reject(err.response.data) })
   }
 
-  static async getUser (id) {
+  async getUser (id) {
     return api().get(`users/${id}`, {
       headers: {
-        Authorization: AuthService.accessToken
+        Authorization: auth.accessToken
       }
     })
     .then((res) => { return Promise.resolve(res.data.user) })
     .catch(err => { return Promise.reject(err.response.data) })
   }
 
-  static async deleteUser (id) {
+  async deleteUser (id) {
     return api().delete(`users/${id}`, {
       headers: {
-        Authorization: AuthService.accessToken
+        Authorization: auth.accessToken
       }
     })
     .then((res) => { return Promise.resolve(res.data) })
     .catch(err => { return Promise.reject(err.response.data) })
   }
 
-  static async getUsers ({page = 1, perPage = 30, sortBy = 'createdAt', order = 'desc', role = 'all'}) {
+  async getUsers ({page = 1, perPage = 30, sortBy = 'createdAt', order = 'desc', role = 'all'}) {
     return api()
       .get('users', {
         params: {
@@ -54,10 +54,13 @@ export default class User {
         },
 
         headers: {
-          Authorization: AuthService.accessToken
+          Authorization: auth.accessToken
         }
       })
       .then((res) => { return Promise.resolve(res.data) })
       .catch((err) => { return Promise.reject(err.response.data) })
   }
 }
+
+const user = new User()
+export default user
--- src/services/rpms/ward/index.js
+++ src/services/rpms/ward/index.js
@@ -1,9 +1,55 @@
 import api from '../api'
-import AuthService from '../auth'
+import auth from '../auth'
 
 class Ward {
-  async create (number, name) {
+  async create (payload) {
+    return api().post('wards', payload, {
+      headers: {
+        authorization: auth.accessToken
+      }
+    }).then((res) => {
+      return Promise.resolve(res.data.ward)
+    })
+    .catch(err => {
+      return Promise.reject(err.response.data)
+    })
+  }
 
+  async getWards () {
+    return api().get('wards?perPage=-1', {
+      headers: {
+        authorization: auth.accessToken
+      }
+    }).then((res) => {
+      return Promise.resolve(res.data.wards)
+    })
+    .catch(err => {
+      return Promise.reject(err.response.data)
+    })
+  }
+
+  async deleteWard (id) {
+    return api().delete(`wards/${id}`, {
+      headers: {
+        authorization: auth.accessToken
+      }
+    }).then((res) => {
+      return Promise.resolve(res.data)
+    }).catch(err => {
+      return Promise.reject(err.response.data)
+    })
+  }
+
+  async updateWard (id, payload) {
+    return api().put(`wards/${id}`, payload, {
+      headers: {
+        authorization: auth.accessToken
+      }
+    }).then((res) => {
+      return Promise.resolve(res.data.ward)
+    }).catch(err => {
+      return Promise.reject(err.response.data)
+    })
   }
 }
 
