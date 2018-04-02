3b59f41a73dd983873b97e2fe502ed41491b7d12
Kasun Vithanage <alankasun@gmail.com>
2018-04-02 23:41:48 +0530

    basic notifcation menu

--- src/store/shared/mutation-types.js
+++ src/store/shared/mutation-types.js
@@ -3,3 +3,5 @@
 export const SET_LAYOUT = 'SET_LAYOUT' // set layout for app
 export const SET_LOADING = 'SET_LOADING' // sets app loading state
 export const SET_SNACKBAR = 'SET_SNACKBAR' // sets the snackbar
+export const PUSH_NOTIFICATION = 'PUSH_NOTIFICATION' // pushes a notification
+export const PUSH_NOTIFICATIONS = 'PUSH_NOTIFICATIONS' // pushes a notification
